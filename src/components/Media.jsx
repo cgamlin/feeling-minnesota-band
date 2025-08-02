b41ff9fec042cd543b68b92752461d4ab79ddd49
Chris Gamlin <cgamlin@revation.com>
2025-08-02 14:46:06 -0500

    Add initial embedded video

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -5,6 +5,16 @@ export default function Media() {
     <div className="media">
       <h2>Media</h2>
       <p>Check out our latest music and videos!</p>
+      <div className="video-container">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/yPr_JDXlBbU"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
